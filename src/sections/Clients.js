811af3e479180aef7aa3c7fd06b84f6fc5e2e551
i-Moe <125210115+i-Moe@users.noreply.github.com>
2024-10-31 20:56:02 +0100

    Update Clients.js

--- src/sections/Clients.js
+++ src/sections/Clients.js
@@ -3,6 +3,7 @@ import React from "react";
 import HalfMoonDivider2 from "../assets/something/half-moon2.svg"; // Example icons from react-icons
 import WorldMap from "../assets/images/map.svg";
 import "flag-icons/css/flag-icons.min.css";
+import FadeIn from "../components/FadeIn";
 // import { GiMayanPyramid } from "react-icons/gi";
 
 const hostels = [
@@ -22,33 +23,43 @@ const Clients = () => {
         className="pt-40 pb-16 bg-lemon relative z-[-110] mt-[-150px]"
       >
         <div className="container-max-w-5xl">
-          <h2 className="text-3xl font-bold text-center my-14">Our Clients</h2>
-          <img src={WorldMap} alt="WorldMap" className="absolute top-[150px] z-[-10] w-11/12 h-auto left-1/2 transform -translate-x-1/2" />
-          <div className="flex flex-wrap justify-center px-2 sm:px-4 md:px-6 lg:px-0">
-            {hostels.map((hostel, index) => (
-              <div
-                key={index}
-                className="w-full sm:w-5/6 md:w-1/2 lg:w-1/3 p-3"
-              >
-                <div className="bg-cream p-5 rounded-xl shadow-solid border border-choco text-center md:min-h-[140px] flex flex-col justify-between">
-                  {/* This div flex container centers the hostel name */}
-                  <div className="flex flex-col flex-grow justify-center">
-                    <h3 className="text-xl font-semibold flex items-center justify-center">
-                      <span
-                        className={`fi fi-${hostel.countryCode.toLowerCase()} mr-2 text-lg`}
-                      />
-                      {hostel.name}
-                      <span
-                        className={`fi fi-${hostel.countryCode.toLowerCase()} ml-2 text-lg`}
-                      />
-                    </h3>
+          <FadeIn>
+            <h2 className="text-3xl font-bold text-center my-14">
+              Our Clients
+            </h2>
+          </FadeIn>
+          <img
+            src={WorldMap}
+            alt="WorldMap"
+            className="absolute top-[150px] z-[-10] w-11/12 max-w-[1000px] h-auto left-1/2 transform -translate-x-1/2"
+          />
+          <FadeIn>
+            <div className="flex flex-wrap justify-center px-2 sm:px-4 md:px-6 lg:px-0">
+              {hostels.map((hostel, index) => (
+                <div
+                  key={index}
+                  className="w-full sm:w-5/6 md:w-1/2 lg:w-1/3 p-3"
+                >
+                  <div className="bg-cream p-5 rounded-xl shadow-solid border border-choco text-center md:min-h-[140px] flex flex-col justify-between">
+                    {/* This div flex container centers the hostel name */}
+                    <div className="flex flex-col flex-grow justify-center">
+                      <h3 className="text-xl font-semibold flex items-center justify-center">
+                        <span
+                          className={`fi fi-${hostel.countryCode.toLowerCase()} mr-2 text-lg`}
+                        />
+                        {hostel.name}
+                        <span
+                          className={`fi fi-${hostel.countryCode.toLowerCase()} ml-2 text-lg`}
+                        />
+                      </h3>
+                    </div>
+                    {/* Country name anchored at the bottom */}
+                    <p className="mt-2">{hostel.country}</p>
                   </div>
-                  {/* Country name anchored at the bottom */}
-                  <p className="mt-2">{hostel.country}</p>
                 </div>
-              </div>
-            ))}
-          </div>
+              ))}
+            </div>
+          </FadeIn>
 
           {/* <div className="flex flex-wrap -mx-4"> */}
           {/* Graphic Design Card */}
